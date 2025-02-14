3eef5a0bfc0fce222a82950889e252391b2e4c4e
Nikitikhonov <nikitikhonov@gmail.com>
2025-02-14 21:30:41 +0200

    mistakes

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 let array = ["abc", 23];
 
-function some(array, evenNumber) {
-    res = 0;
+function some(array, fun) {
     for(let i = 0; i < array.length; i++) {
-        if (evenNumber()) {
-            res = true;
+        if (fun(array[i])) {
+            return true;
         } else {
-            res = false;
+            return false;
         }
-    } 
-    return res;     
+    }    
 }
 
 function evenNumber(num) {
-    for (let i = 0; i < array.length; i++) {
-        num = array[i];
-    }
+    typeof num == 'number';
     return num % 2 == 0;
-}
+}   
 
-console.log(`using "some" function for even numbers array: ${array}, result: ${some(array, evenNumber)} - false`)
+
+console.log(`using "some" function for even numbers array: ${array} => result: ${some(array, evenNumber)} - false`)
 //returns true if at least one element of the array match a condition given in the function (fun)
 
 let array1 = [2, 3, 4];
 
-function every(array1, elmGreaterIndex) {
-    res = 0;
+function every(array1, fun) {
     for(let i = 0; i < array1.length; i++) {
-        if (elmGreaterIndex()) {
-            res = true;
+        if (!fun(array1[i], i)) {
+            return true;
         } else {
-            res = false;
+            return false;
         }
     }
-    return res;
 }
 
 function elmGreaterIndex(elem, index) {
-    for (let i = 0; i < array1.length; i++) {
-        elem = array1[i];
-        index = i;
-    }
     return elem > index;
 }
 
-console.log(`using "every" function for elements greater than the index values, array: ${array1}, result: ${every(array1, elmGreaterIndex)} - true`)
-//returns true if all elemnts of the array match a condition given in the function (fun)
+console.log(`using "every" function for elements greater than the index values, array: ${array1} => result: ${every(array, evenNumber)} - true`)
+// //returns true if all elemnts of the array match a condition given in the function (fun)
